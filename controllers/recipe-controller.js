862a3beebdb70a7331e90f2291224d51092cbcc7
AiyappaND <vinayaiyappa24816@gmail.com>
2023-04-19 21:27:17 -0400

    Add descending sort for latest recipes

--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -31,10 +31,17 @@ const getOneRecipe = async (req, res) => {
     res.json(foundRecipe);
 }
 
+const getLocalMatchingRecipe = async (req, res) => {
+    const recipeName = req.params['name'];
+    const recipesFound = await recipeDao.findLocalRecipeByName(recipeName);
+    res.json(recipesFound);
+}
+
 export default (app) => {
     app.post('/api/recipes', createRecipe);
     app.get('/api/recipes', findRecipes);
     app.put('/api/recipes/:rid', updateRecipe);
     app.delete('/api/recipes/:rid', deleteRecipe);
-    app.get('/api/recipes/:rid', getOneRecipe);
+    app.get('/api/recipes/getOne/:rid', getOneRecipe);
+    app.get('/api/recipes/findLocal/:name', getLocalMatchingRecipe);
 }
